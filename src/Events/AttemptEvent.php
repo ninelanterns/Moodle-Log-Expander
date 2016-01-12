c94906d6b1647d5d4479c568f37cc7784538965b
Andrew Downes <mrdownes@hotmail.com>
2016-01-12 13:39:32 +0000

    Get grade information

--- src/Events/AttemptEvent.php
+++ src/Events/AttemptEvent.php
@@ -8,10 +8,15 @@ class AttemptEvent extends Event {
      * @override Event
      */
     public function read(array $opts) {
+        global $DB;
         $attempt = $this->repo->readAttempt($opts['objectid']);
+
+        $gradeitems = $DB->get_record('grade_items', array('itemmodule' => 'quiz', 'iteminstance' =>$attempt->quiz));
+
         return array_merge(parent::read($opts), [
             'attempt' => $attempt,
             'module' => $this->repo->readModule($attempt->quiz, 'quiz'),
+            'gradeitems' => $gradeitems
         ]);
     }
 }
