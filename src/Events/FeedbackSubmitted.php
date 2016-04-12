0dd0ebf00f651382a318d5c3f3fecc9e93dd5a42
Andrew Downes <mrdownes@hotmail.com>
2016-04-12 13:50:47 +0100

    avoid conflict with #30

--- src/Events/FeedbackSubmitted.php
+++ src/Events/FeedbackSubmitted.php
@@ -1,6 +1,6 @@
 <?php namespace LogExpander\Events;
 
-class FeedbackSubmitted extends Event {
+class FeedbackSubmitted extends ModuleEvent {
     /**
      * Reads data for an event.
      * @param [String => Mixed] $opts
@@ -9,12 +9,28 @@ class FeedbackSubmitted extends Event {
      */
     public function read(array $opts) {
 
+        $parent = parent::read($opts);
+        if (is_null($parent)) {
+            return null;
+        }
+
         $attempt = $this->repo->readFeedbackAttempt($opts['objectid']); 
+        if ($attempt === false) {
+            return null;
+        }
+        $questions = $this->repo->readFeedbackQuestions($attempt->feedback);
+        $module = $this->repo->readModule($attempt->feedback, 'feedback');
+        if (
+            ($questions === false) 
+            || ($module === false) 
+        ){
+            return null;
+        }
 
         return array_merge(parent::read($opts), [
-            'module' => $this->repo->readModule($attempt->feedback, 'feedback'),
-            'questions' => $this->repo->readFeedbackQuestions($attempt->feedback),
+            'questions' => $questions,
             'attempt' => $attempt,
+            'module' => $module
         ]);
     }
 }
