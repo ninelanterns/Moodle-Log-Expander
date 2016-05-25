c1b4127cd78038189f59171eeb394d8609c3d76c
lee.kirkland <lee.kirkland@asmr.com>
2016-05-25 19:13:52 -0400

    Extending Event.php.

--- src/Events/CourseCompleted.php
+++ src/Events/CourseCompleted.php
@@ -18,17 +18,7 @@ class CourseCompleted extends Event
      * @override Event
      */
     public function read(array $opts) {
-        $version = str_replace("\n", "", str_replace("\r", "", file_get_contents(__DIR__.'/../../VERSION')));
-        return [
-            'user' => $opts['relateduserid'] < 1 ? null : $this->repo->readUser($opts['relateduserid']),
-            'relateduser' => $opts['relateduserid'] < 1 ? null : $this->repo->readUser($opts['relateduserid']),
-            'course' => $this->repo->readCourse($opts['courseid']),
-            'app' => $this->repo->readCourse(1),
-            'info' => (object) [
-                'https://moodle.org/' => $this->repo->readRelease(),
-                'https://github.com/LearningLocker/Moodle-Log-Expander' => $version,
-            ],
-            'event' => $opts,
-        ];
+        return array_merge(parent::read($opts), [
+        ]);
     }
 }
