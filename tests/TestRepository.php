7f210fcfbc449159110504372c11af562671976a
Andrew Downes <mrdownes@hotmail.com>
2016-07-07 16:47:48 +0100

    start improvements to test repo

--- tests/TestRepository.php
+++ tests/TestRepository.php
@@ -10,22 +10,247 @@ class TestRepository extends MoodleRepository {
      * @override MoodleRepository
      */
     protected function readStoreRecord($type, array $query) {
-         $response = [
+        $response; 
+
+        switch ($type) {
+            case 'user':
+            $reponse = [
+                'id' => '1',
+                'username' => 'test_username',
+                'lang' => 'en',
+                'fullname' => 'test_fullname',
+                'password' => 'topsecret'
+            ]
+                break;
+
+            case 'assign_grades':
+            $reponse = [
+                'id' => '1',
+                'assignment' => '1',
+                'userid' => '1',
+                'timecreated' => 1433946702,
+                'timemodified' => 1433946702,
+                'grade' => '2',
+            ]
+                break;
+
+            case 'assign_submission':
+            $reponse = [
+                'id' => '1',
+                'assignment' => '1',
+                'userid' => '1',
+                'timecreated' => 1433946702,
+                'timemodified' => 1433946702,
+            ]
+                break;
+
+            case 'quiz_attempts':
+            $reponse = [
+                'id' => '1',
+                'quiz' => '1',
+                'userid' => '1',
+                'timemodified' => 1433946702,
+            ]
+                break;
+
+            case 'forum_discussions':
+            $reponse = [
+                'id' => '1',
+                'course' => '1',
+                'forum' => '1',
+                'name' => 'test_name',
+                'userid' => '1',
+                'timemodified' => 1433946702,
+            ]
+                break;
+
+            case 'facetoface_sessions':
+            $reponse = [
+                'id' => '1',
+                'facetoface' => '1',
+                'timecreated' => 1433946702,
+                'timemodified' => 1433946702,
+            ]
+                break;
+
+            case 'feedback_completed':
+            $reponse = [
+                'id' => '1',
+                'feedback' => '1',
+                'userid' => '1',
+                'timemodified' => 1433946702,
+            ]
+                break;
+
+            case 'page':
+            $reponse = [
+                'id' => '1',
+                'course' => '1',
+                'name' => 'test_name',
+                'timemodified' => 1433946702,
+            ]
+                break;
+
+            case 'scorm_scoes':
+            $reponse = [
+                'id' => '1',
+                'scorm' => '1'
+            ]
+                break;
+
+            case 'modules':
+            $reponse = [
+                'id' => '1',
+                'name' => 'test_name'
+            ]
+                break;
+
+            case 'course_modules':
+            $reponse = [
+                'id' => '1',
+                'course' => '1',
+                'module' => '1',
+                'instance' => '1'
+            ]
+                break;
+
+            case 'question_attempts':
+            $reponse = [
+                'id' => '1',
+                'questionid' => '1',
+                'variant' => '1',
+                'rightanswer' => 'test answer',
+                'responsesummary' => 'test answer',
+                'timemodified' => 1433946702,
+            ]
+                break;
+
+            case 'question_attempt_steps':
+            $reponse = [
+                'id' => '1',
+                'state' => 'gradedright',
+                'fraction' => '1.0000',
+                'timecreated' => 1433946702,
+                'userid' => '1',
+            ]
+                break;
+
+            case 'question_attempt_step_data':
+            $reponse = [
+                'id' => '1',
+                'name' => 'test_name',
+                'value' =>  '2',
+            ]
+                break;
+
+            case 'quiz_slots':
+            $reponse = [
+                'id' => '1',
+                'slot' => '1',
+                'quizid' => '1',
+                'questionid' => '1',
+                'maxmark' => '5.00000',
+            ]
+                break;
+
+            case 'question_answers':
+            $reponse = [
+                'id' => '1',
+                'question' => '1',
+                'answer' => '1',
+                'fraction' => '1.0000',
+            ]
+                break;
+
+            case 'grade_items':
+            $reponse = [
+                'id' => '1',
+                'courseid' => '1',
+                'grademax' => '5.00000',
+                'grademin' => '0.00000',
+                'gradepass' => '5.00000',
+                'timecreated' => 1433946702,
+                'timemodified' => 1433946702,
+            ]
+                break;
+
+            case 'assignfeedback_comments':
+            $reponse = [
+                'id' => '1',
+                'assignment' => '1',
+                'grade' => '1',
+                'commenttext' => '<p>test comment</p>',
+            ]
+                break;
+
+            case 'feedback_completed':
+            $reponse = [
+                'id' => '1',
+                'feedback' => '1',
+                'userid' => '1',
+            ]
+                break;
+
+            case 'feedback_value':
+            $reponse = [
+                'id' => '1',
+                'course_id' => '1',
+                'item' => '1'
+            ]
+                break;
+
+            case 'feedback_item':
+            $reponse = [
+                'id' => '1',
+                'feedback' => '1',
+                'name' => 'test_name',
+                'presentation' => 'r>>>>>0#### incorrect|1#### correct',
+                'typ' => 'multichoicerated',
+            ]
+                break;
+
+            case 'feedback_template':
+            $reponse = [
+                'id' => '1',
+                'course' => '1',
+                'name' => 'test_name'
+            ]
+                break;
+
+            case 'course':
+            $reponse = [
+                'id' => '1',
+                'category' => '1',
+                'fullname' => 'test_name',
+                'shortname' => 'test_name',
+                'summary' => 'test_summary',
+                'lang' => 'en',
+                'timecreated' => 1433946702,
+                'timemodified' => 1433946702,
+            ]
+                break;
+
+            // TODO: forum_discussions, facetoface_sessions, facetoface_signups
+            // TODO: check specifc events for other tables
+
+            default:
+                break;
+        }
+
+         /*$response = [
             'id' => '1',
-            'username' => 'test_username',
-            'lang' => 'en',
-            'fullname' => 'test_fullname',
-            'summary' => 'test_summary',
-            'name' => 'test_name',
+            
+            
+            
             'intro' => 'test_intro',
             'timestart' => 1433946701,
             'timefinish' => 1433946702,
             'timestamp' => 1433946702,
-            'timecreated' => 1433946702,
-            'state' => 'finished',
+            
+            
             'course' => '1',
             'sumgrades' => '1',
-            'grade' => '2',
+            
             'quiz' => '1',
             'assignment' => '1',
             'facetoface' => '1',
@@ -35,29 +260,21 @@ class TestRepository extends MoodleRepository {
             'scorm' => '1',
             'feedback' => '1',
             'template' => '1',
-            'grademax' => '5.00000',
-            'grademin' => '0.00000',
-            'gradepass' => '5.00000',
-            'commenttext' => '<p>test comment</p>',
+            
             'questionid' => '1',
             'questiontext' => '<p>test question</p>',
             'qtype' => 'multichoice',
-            'maxmark' => '5.00000',
-            'fraction' => '1.0000',
+            
+            
             'answer' => 'test answer',
             'rightanswer' => 'test answer',
             'responsesummary' => 'test answer',
             'sequencenumber' => 1,
             'item' => '1',
-            'presentation' => 'r>>>>>0#### incorrect|1#### correct',
-            'typ' => 'multichoicerated',
-            'value' =>  '2',
-            'timemodified' => 1433946702,
+            
+            '
+            
         ];
-
-        if ($type == 'question_attempt_steps') {
-            $response['state'] = 'gradedright';
-        }
 
         return (object) $response;
     }
