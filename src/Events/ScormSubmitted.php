6f64465d0e4c4ce91735ce383993d412b7fd6c10
Jerrett Fowler <jerrett.fowler@gmail.com>
2016-07-21 17:04:35 +0100

    Scorm Submitted Test added

--- src/Events/ScormSubmitted.php
+++ src/Events/ScormSubmitted.php
@@ -1,29 +1,5 @@
 <?php namespace LogExpander\Events;
 
-/*
-id:	576
-eventname: \mod_scorm\event\scoreraw_submitted
-component: mod_scorm
-action:	submitted
-target:	scoreraw
-objecttable:	scorm_scoes_track
-objectid:	2
-crud:	u
-edulevel:	2
-contextid: 125
-contextlevel:	70
-contextinstanceid: 4
-userid:	2
-courseid:	15
-relateduserid: 2
-anonymous:	0
-other: a:3:{s:9:"attemptid";i:2;s:10:"cmielement";s:18:"cmi.core.score.raw";s:8:"cmivalue";s:1:"0";}
-timecreated: 1467386941
-origin:	web
-ip:	217.155.37.242
-realuserid: NULL
-*/
-
 class ScormSubmitted extends Event {
     /**
      * Reads data for an event.
@@ -33,10 +9,10 @@ class ScormSubmitted extends Event {
      */
     public function read(array $opts) {
         $cmi_unserialized = unserialize($opts['other']);
-        $scoid = $opts->contextinstanceid;
-        $scormid = $opts->objectid;
+        $scoid = $opts['contextinstanceid'];
+        $scormid = $opts['objectid'];
         $attempt = $cmi_unserialized['attemptid'];
-        $scorm_scoes_track = $this->repo->readScormScoesTrack($opts->userid,
+        $scorm_scoes_track = $this->repo->readScormScoesTrack($opts['userid'],
                                                               $scormid,
                                                               $scoid,
                                                               $attempt);
--- tests/ScormSubmittedTest.php
+++ tests/ScormSubmittedTest.php
@@ -14,6 +14,7 @@ class ScormSubmittedTest extends EventTest {
         return array_merge(parent::constructInput(), [
             'objecttable' => 'scorm_scoes_track',
             'objectid' => 1,
+            'contextinstanceid' => 1,
             'eventname' => '\mod_scorm\event\scoreraw_submitted',
             'other' => 'a:3:{s:9:"attemptid";i:1;s:10:"cmielement";s:18:"cmi.core.score.raw";s:8:"cmivalue";s:1:"0";}',
         ]);
@@ -22,12 +23,21 @@ class ScormSubmittedTest extends EventTest {
     protected function assertOutput($input, $output) {
         parent::assertOutput($input, $output);
         echo(print_r($output, true));
-        //$this->assertEquals("1", $output["1"]->id);
         $this->assertModule(1, $output['module'], 'scorm');
-        $this->assertScorm(1, $output['scorm_scoes_track']);
+        $this->assertEquals(1, $output['scorm_scoes']->id);
+        $this->assertScormScoesTrack($input, $output);
+        $this->assertCmiData($input, $output);
     }
 
-    protected function assertScorm($input, $output) {
-        $this->assertRecord($input, $output);
+    protected function assertScormScoesTrack($input, $output) {
+        $this->assertEquals('status', $output['scorm_scoes_track']['status']);
+        $this->assertEquals(100, $output['scorm_scoes_track']['scoremax']);
+        $this->assertEquals(0, $output['scorm_scoes_track']['scoremin']);
+    }
+
+    protected function assertCmiData($input, $output) {
+        $this->assertEquals(1, $output['cmi_data']['attemptid']);
+        $this->assertEquals('cmi.core.score.raw', $output['cmi_data']['cmielement']);
+        $this->assertEquals(0, $output['cmi_data']['cmivalue']);
     }
 }
