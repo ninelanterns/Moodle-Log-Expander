e45e7ac364321bfb75cbde3c8c9845e97c4ccc0b
Andrew Downes <mrdownes@hotmail.com>
2016-07-21 09:41:14 +0100

    single quotes

--- tests/AttemptEventTest.php
+++ tests/AttemptEventTest.php
@@ -31,31 +31,31 @@ class AttemptEventTest extends EventTest {
     }
 
     protected function assertQuestionAttempts($output) {
-        $this->assertEquals("1", $output["1"]->id);
-        $this->assertEquals("2", $output["1"]->steps["2"]->id);
-        $this->assertEquals("2", $output["1"]->steps["1"]->data["2"]->id);
+        $this->assertEquals('1', $output['1']->id);
+        $this->assertEquals('2', $output['1']->steps['2']->id);
+        $this->assertEquals('2', $output['1']->steps['1']->data['2']->id);
     }
 
     protected function assertQuestions($output) {
-        $this->assertEquals("multichoice", $output["1"]->qtype);
-        $this->assertEquals("1", $output["1"]->id);
-        $this->assertEquals("1", $output["1"]->answers["1"]->id);
-        $this->assertEquals($this->cfg->wwwroot . '/mod/question/question.php?id=1', $output["1"]->url);
+        $this->assertEquals('multichoice', $output['1']->qtype);
+        $this->assertEquals('1', $output['1']->id);
+        $this->assertEquals('1', $output['1']->answers['1']->id);
+        $this->assertEquals($this->cfg->wwwroot . '/mod/question/question.php?id=1', $output['1']->url);
 
-        $this->assertEquals("numerical", $output["2"]->qtype);
-        $this->assertEquals("1", $output["2"]->numerical->answers["1"]->id);
-        $this->assertEquals("1", $output["2"]->numerical->options->id);
-        $this->assertEquals("1", $output["2"]->numerical->units["1"]->id);
+        $this->assertEquals('numerical', $output['2']->qtype);
+        $this->assertEquals('1', $output['2']->numerical->answers['1']->id);
+        $this->assertEquals('1', $output['2']->numerical->options->id);
+        $this->assertEquals('1', $output['2']->numerical->units['1']->id);
 
-        $this->assertEquals("match", $output["3"]->qtype);
-        $this->assertEquals("1", $output["3"]->match->options->id);
-        $this->assertEquals("1", $output["3"]->match->subquestions["1"]->id);
+        $this->assertEquals('match', $output['3']->qtype);
+        $this->assertEquals('1', $output['3']->match->options->id);
+        $this->assertEquals('1', $output['3']->match->subquestions['1']->id);
 
-        $this->assertEquals("calculated", $output["4"]->qtype);
-        $this->assertEquals("1", $output["4"]->calculated->answers["1"]->id);
-        $this->assertEquals("1", $output["4"]->calculated->options->id);
+        $this->assertEquals('calculated', $output['4']->qtype);
+        $this->assertEquals('1', $output['4']->calculated->answers['1']->id);
+        $this->assertEquals('1', $output['4']->calculated->options->id);
 
-        $this->assertEquals("shortanswer", $output["5"]->qtype);
-        $this->assertEquals("1", $output["5"]->shortanswer->options->id);
+        $this->assertEquals('shortanswer', $output['5']->qtype);
+        $this->assertEquals('1', $output['5']->shortanswer->options->id);
     }
 }
